87f78b2311e64b67ac6e7b6cff706cd5221e01b6
Om Rajpal <rajpalom13274@gmail.com>
2024-04-07 01:11:50 +0530

    app frontend

---
+++ main/App/hackbyte/lib/chat.dart
@@ -0,0 +1,261 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'package:http/http.dart' as http;
+
+class Chat {
+  final String id;
+  final String subject;
+  final String message;
+
+  Chat(this.id, this.subject, this.message);
+
+  factory Chat.fromJson(Map<String, dynamic> json) {
+    return Chat(
+      json['_id'],
+      json['subject'],
+      json['message'],
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final dynamic token;
+  final dynamic uid;
+  const HomePage({@required this.token, @required this.uid, super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Chat> chats = [];
+
+  late String username = "";
+  late String id;
+  late io.Socket socket;
+
+  @override
+  void initState() {
+    fetchChats();
+    socket = io.io('http://192.168.102.51:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      "autoConnect": false,
+    });
+    socket.onConnect((_) {
+      print('connected');
+    });
+
+    socket.connect();
+
+    print(socket.connected);
+    if (widget.token != null) {
+      Map<String, dynamic> jwtDecodedtoken = JwtDecoder.decode(widget.token);
+      username = jwtDecodedtoken['username'] ?? "";
+      id = jwtDecodedtoken['_id'];
+    }
+
+    super.initState();
+  }
+
+  TextEditingController chat = TextEditingController();
+  TextEditingController subjectCont = TextEditingController();
+
+  void sendChat(String id, String status, String subject, String message,
+      String room) async {
+    if (chat.text.isNotEmpty && subjectCont.text.isNotEmpty) {
+      var reqBody = {
+        "_id": id,
+        "status": status,
+        "subject": subject,
+        "message": message,
+        "room": room
+      };
+
+      var response = await http.post(
+          Uri.parse('http://192.168.102.51:3000/chat'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(reqBody));
+
+      if (response.statusCode == 200) {
+        // Emit the newChat event to notify the server
+        socket.emit('new', 'rebuild');
+
+        socket.on("build", (data) {
+          fetchChats();
+        });
+      }
+    }
+  }
+
+  Future<void> fetchChats() async {
+    var reqBody = {
+      "room": widget.uid,
+    };
+    var response = await http.post(
+        Uri.parse('http://192.168.102.51:3000/fetch'),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(reqBody));
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(response.body);
+      setState(() {
+        chats = responseData
+            .map((data) => Chat.fromJson(data))
+            .where((chat) => chat.id == id)
+            .toList();
+      });
+    } else {
+      throw Exception('Failed to load chats');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.black,
+        child: Column(
+          children: [
+            Expanded(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset(
+                      'assets/gif/chat.gif',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Text(
+                    'Room ${widget.uid}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 9,
+              child: ListView.builder(
+                itemCount: chats.length,
+                itemBuilder: (context, index) {
+                  final chat = chats[index];
+                  return ListTile(
+                    title: Text(
+                      chat.subject,
+                      style: const TextStyle(
+                          color: Color.fromARGB(255, 157, 157, 157),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      chat.message,
+                      style: const TextStyle(color: Colors.white, fontSize: 13),
+                    ),
+                  );
+                },
+              ),
+            ),  
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 10, right: 10, bottom: 5, top: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      style: const TextStyle(color: Colors.white, fontSize: 14),
+                      cursorColor: Colors.white,
+                      cursorWidth: 2,
+                      textAlignVertical: TextAlignVertical.center,
+                      cursorHeight: 20,
+                      cursorOpacityAnimates: true,
+                      cursorRadius: const Radius.circular(20),
+                      decoration: InputDecoration(
+                        labelText: "Subject",
+                        labelStyle: const TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Roboto',
+                            fontSize: 13),
+                        fillColor: const Color.fromARGB(255, 67, 67, 67),
+                        filled: true,
+                        contentPadding: const EdgeInsets.all(10),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                      controller: subjectCont,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: TextField(
+                      style: const TextStyle(color: Colors.white, fontSize: 14),
+                      cursorColor: Colors.white,
+                      cursorWidth: 2,
+                      textAlignVertical: TextAlignVertical.center,
+                      cursorHeight: 20,
+                      cursorOpacityAnimates: true,
+                      cursorRadius: const Radius.circular(20),
+                      decoration: InputDecoration(
+                        labelText: "Enter the message",
+                        labelStyle: const TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Roboto',
+                            fontSize: 13),
+                        fillColor: const Color.fromARGB(255, 67, 67, 67),
+                        filled: true,
+                        contentPadding: const EdgeInsets.all(10),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                      controller: chat,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 1,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        sendChat(id, "pending", subjectCont.text, chat.text,
+                            widget.uid);
+                        chat.text = "";
+                        fetchChats();
+                        setState(() {});
+                      },
+                      icon: const Icon(
+                        Icons.send,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ main/App/hackbyte/lib/welcome.dart
@@ -0,0 +1,241 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:hackbyte/chat.dart';
+import 'package:hackbyte/dashboard.dart';
+import 'package:hackbyte/gifAnim.dart';
+import 'package:hackbyte/login.dart';
+import 'package:hackbyte/name.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:lottie/lottie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class Welcome extends StatefulWidget {
+  final dynamic token;
+
+  const Welcome({required this.token, super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  late SharedPreferences prefs;
+
+  void EnterRoom(BuildContext context) async {
+    if (room.text.isNotEmpty) {
+      var reqBody = {
+        "room": room.text.trim(),
+      };
+
+      var response = await http.post(
+          Uri.parse('http://192.168.102.51:3000/room'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(reqBody));
+
+      var jsonRes = jsonDecode(response.body);
+      if (jsonRes['status']) {
+        print("kya baat hai");
+        var UID = jsonRes['UID'];
+        print(UID);
+        await prefs.setString('UID', UID);
+        if (!context.mounted) return;
+
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomePage(
+                token: widget.token,
+                uid: UID,
+              ),
+            ));
+      }
+    }
+  }
+
+  void logout(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+    if (!context.mounted) return;
+
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const Login(),
+        ));
+  }
+
+  TextEditingController room = TextEditingController();
+  late String username;
+
+  @override
+  void initState() {
+    initSharedPreferences();
+    super.initState();
+    Map<String, dynamic> jwtDecodedtoken = JwtDecoder.decode(widget.token);
+    username = jwtDecodedtoken['username'];
+  }
+
+  void initSharedPreferences() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.black,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const GifAnim(gifPath: 'assets/gif/dashboard.gif'),
+                    Stats(
+                      username: username,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.70,
+                        height: 45,
+                        child: TextField(
+                          style:
+                              const TextStyle(color: Colors.white, fontSize: 14),
+                          cursorColor: Colors.white,
+                          cursorWidth: 2,
+                          textAlignVertical: TextAlignVertical.center,
+                          cursorHeight: 20,
+                          cursorOpacityAnimates: true,
+                          cursorRadius: const Radius.circular(20),
+                          decoration: InputDecoration(
+                            labelText: "Enter a room code",
+                            labelStyle: const TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Roboto',
+                              fontSize: 13,
+                            ),
+                            fillColor: const Color.fromARGB(255, 67, 67, 67),
+                            filled: true,
+                            contentPadding: const EdgeInsets.all(10),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(
+                                  color: Colors.white), // Added border color
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: Colors.white), // Added border color
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: const BorderSide(
+                                  color: Colors.white), // Added border color
+                            ),
+                          ),
+                          controller: room,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          EnterRoom(context);
+                        },
+                        style: ButtonStyle(
+                            alignment: Alignment.center,
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                const Color.fromARGB(255, 67, 67, 67)),
+                            shape:
+                                MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(12)))),
+                        child: const Text(
+                          'Join Room',
+                          style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontSize: 13,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 15,
+              left: 15,
+              child: InkWell(
+                onTap: () {
+                  logout(context);
+                },
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 93, 93, 93),
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                    SizedBox(
+                        width: 10,
+                        height: 10,
+                        child: Image.asset('assets/icons/logout.png'))
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              right: 50,
+              bottom: 50,
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Dashboard(token: widget.token),
+                        ));
+                  },
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 85, 85, 85),
+                            borderRadius: BorderRadius.circular(25)),
+                      ),
+                      SizedBox(
+                        width: 35,
+                        height: 35,
+                        child: Lottie.asset(
+                          'assets/gif/rocket.json',
+                          fit: BoxFit.contain,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
